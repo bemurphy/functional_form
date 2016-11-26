24e07bfdef57e5bd8dc040f49376b5d6488d0b56
Brendon Murphy <xternal1+github@gmail.com>
2016-11-25 20:45:27 -0800

    Extract routing into src/router

---
+++ src/router/index.js
@@ -0,0 +1,32 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Form from '../components/Form.vue';
+import ShowFormSubmission from '../components/ShowFormSubmission.vue';
+import FormSetup from '../components/FormSetup.vue';
+
+Vue.use(VueRouter);
+
+function StubComponent(name) {
+  return { template: '<div class="container"><div class="row"><div class="col-md-12">' + name + '#todo</div></div></div>' };
+}
+
+const routes = [
+  {
+    path: '/forms/:form_id', component: Form,
+    children: [
+      { path: 'submissions/:id', component: ShowFormSubmission, name: 'ShowFormSubmission' },
+      { path: 'submissions', redirect: to => {
+        return 'submissions/1';
+      }, name: 'FormSubmissions' },
+      { path: 'setup', component: FormSetup, name: 'FormSetup' },
+      { path: 'edit', component: StubComponent('FormEdit'), name: 'FormEdit' },
+      { path: 'emails', component: StubComponent('FormEmails'), name: 'FormEmails' },
+      { path: 'export', component: StubComponent('FormExport'), name: 'FormExport' },
+      { path: 'webhooks', component: StubComponent('FormWebhooks'), name: 'FormWebhooks' },
+    ]
+  },
+];
+
+export default new VueRouter({
+  routes: routes
+});
