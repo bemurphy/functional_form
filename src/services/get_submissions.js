52d8f6d7d7684bf06760de95b18d6429babca6e1
Brendon Murphy <xternal1+github@gmail.com>
2016-11-26 10:09:05 -0800

    Remove submissions generation code
    This is handled by dev REST backend api now

--- src/services/get_submissions.js
+++ src/services/get_submissions.js
@@ -1,22 +1,3 @@
-import faker from 'faker';
-
-function generateSubmissions() {
-  let s = [];
-
-  for (let i=1; i<10; i++) {
-    s.push({
-      id: i,
-      email: faker.internet.email().toLowerCase(),
-      avatar: faker.internet.avatar(),
-      name: faker.name.findName(),
-      phone_number: faker.phone.phoneNumber(),
-      bio: faker.lorem.sentence()
-    });
-  }
-
-  return s;
-}
-
 module.exports = function(formId, callback) {
   console.log('getting submissions for formId', formId);
 
